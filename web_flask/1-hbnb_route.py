4ae6ccc8780657bf5ab64e81c995e4440046250c
Harrison <harrisonchialuka@gmail.com>
2024-06-01 22:31:27 +0100

    1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
-
-# module to use flask web framework
+"""  module to use flask web framework """
 
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def greeting():
     """ greet """
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def console():
-    """ Thr console prompt """
+    """ The console prompt """
     return "HBNB"
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
