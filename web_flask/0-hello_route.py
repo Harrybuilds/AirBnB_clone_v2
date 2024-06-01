7ebd0814f7290bea1a385febba80f80ad01d694b
Harrison <harrisonchialuka@gmail.com>
2024-06-01 22:17:03 +0100

    web flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -12,4 +12,5 @@ def greeting():
     return "Hello HBNB!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
