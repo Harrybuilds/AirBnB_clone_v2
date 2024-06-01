d7f4e213120bc55c8da2a44533b0f288ac83ad2d
Harrison <harrisonchialuka@gmail.com>
2024-06-01 22:36:24 +0100

    task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python3
-
-# module to use flask web framework
+#!/usr/bin/python3
+""" module to use flask web framework """
 
 from flask import Flask
 from markupsafe import escape
@@ -8,15 +7,18 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def greeting():
     """ greet """
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def console():
     """ the console """
     return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def handletext(text):
@@ -24,6 +26,7 @@ def handletext(text):
     if "_" in text:
         text = text.replace("_", " ")
     return f"{escape(text)}"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
